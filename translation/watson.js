5fb6d33f868df8e7b5ede56a768c4e9d4c48475f
Mike Woodman <mwoodman@mitre.org>
2022-01-31 14:49:49 -0500

    ACE Quill v1.2.0

--- translation/watson.js
+++ translation/watson.js
@@ -10,7 +10,7 @@
 *                  The MITRE Corporation, Contracts Management Office, 7515 Colshire Drive,
 *                  McLean, VA 22102-7539, (703) 983-6000.
 *
-*                                          ©2018 The MITRE Corporation.
+*                                          ©2022 The MITRE Corporation.
 *                                                          */
 
 const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
